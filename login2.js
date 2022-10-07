4d95b0a507e002e2f8165a23f62fb44483ddc1d7
manmeetkaur0 <112695523+manmeetkaur0@users.noreply.github.com>
2022-10-07 23:07:43 +0530

    my tiffin service website

---
+++ login2.js
@@ -0,0 +1,44 @@
+function fun()
+{
+   var username=document.getElementById("fname").value;
+   var username=document.getElementById("fmail").value;
+   var username=document.getElementById("fphone").value;
+   if(fname=="admin"&&  fmail=="user" && fphone=="fphone")
+   {
+       alert("login successfully");
+       return false;
+   }
+   else
+   alert("login failed");
+}
+
+function fun(){
+    var nam = document.getElementById('fname').value;
+	
+	if(nam == ''){
+		
+		alert('plz write something');
+	
+	}
+	else if(nam.length <3){
+	
+		alert(' pls write minimum three letter')
+	
+	}
+	
+	
+	else if(nam.length > 6){
+		alert('pls write maximum six letter');
+	}
+
+}
+
+
+function fun() {  
+    alert (" In this our  delicious Meals menu will come while you click on this button ");  
+ } 
+
+
+
+
+
